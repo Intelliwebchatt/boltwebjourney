e2a44ee6232dd7e07768dd98472193ad1cd1e82b
Intelliwebchatt <aslockhart10@gmail.com>
2024-11-08 22:12:42 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/[your-repo-name]/',
+  base: '/boltwebjourney/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
 })
